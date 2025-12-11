206f65c20f8adc54e3645186c933df5e3e9e4309
Nwachukwu Jesse Chijioke <jesfusionprox@gmail.com>
2025-12-11 08:32:48 +0100

    Feat: Add code on preprocessing and jargon-bursting in Sklearn to container

--- Scikit-Learn/script_2.py
+++ Scikit-Learn/script_2.py
@@ -87,3 +87,152 @@ print(f'''
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+d_engine = create_engine(postgre_connect)
+
+the_dataset = pd.read_sql(
+    "SELECT * FROM car_prices_raw",
+
+    d_engine
+)
+
+print(f'''
+======================================== Extracted Dataset ========================================
+      
+{the_dataset.head().to_markdown()}
+''')
+
+# ===================================== Defining Concepts in Scikit-Learn and Data Science in General =====================================
+
+# Target/Labels (y): This is what we want to predict (usually a single pandas Series/Column)
+
+target_y = the_dataset["Price"]
+
+print(f'''
+======================================== Target (y), Shape: {target_y.shape} ========================================
+
+{target_y.head().to_markdown()}
+
+It is a Vector (ID)
+''')
+
+# Features (X): this is what the model tries to learn in order to see it's pattern with the targets
+
+feature_x = the_dataset.drop(["Price"], axis = 1)
+
+print(f'''
+======================================== Features (X), Shape: {feature_x.shape} ========================================
+
+{feature_x.head().to_markdown()}
+
+It is a Matrix (2D)
+''')
+
+"""
+WHY PREPROCESSING?
+
+If we were to run this line below, Scikit-learn would CRASH:
+```model.fit(feature_x, target_y)
+
+WHY THIS WOULD FAIL:
+
+1. Look at 'Fuel_Type': It contains strings like 'Electric'.
+
+You cannot do: 'Electric' * 0.5 + 2. The math explodes.
+
+Solution: We need Encoding
+
+2. Look at 'Odometer_KM' vs 'Age_Years':
+Max Odometer: {feature_x['Odometer_KM'].max()}
+Max Age: {feature_x['Age_Years'].max()}
+
+The model will think Odometer is 25,000x more important than Age just because the number is bigger.
+Solution: We need Scaling
+"""
+
+
+
+
+
+
+
+
