6485c50d6af05dfd1a0b66b4df90044a47d72bcd
Nwachukwu Jesse Chijioke <jesfusionprox@gmail.com>
2025-12-13 21:06:58 +0100

    Feat: Add code on path vs. query parameters learnt to container

--- FastAPI/script_2.py
+++ FastAPI/script_2.py
@@ -20,3 +20,127 @@ async def check_health():
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app = FastAPI()
+
+# ===================================== Path vs. Query Parameters =====================================
+
+'''
+Path Parameters points to a specific resource 
+For example: "/items/model_v5"
+- Here we're trying to access the 5th version of our model
+
+Query Parameters modifies how you view the resource. It comes after "?"
+
+For Example: "/predict?user_ID=99&threshold=0.8"
+- Here, we're trying to access the prediction, but only if the user_ID is valid and the confidence is > 80%
+'''
+
+@app.get("/")
+async def home():
+    return "Welcome to FastAPI!"
+
+
+@app.get("/predict/{model}")
+async def churn_predictor(
+
+    model: str, # Path Parameter (matched in {}) 
+    user_ID: int, # Query Parameter (not in {})
+    treshold: float = 0.1, # Query Parameter with Default
+
+): # FastAPI performs type checking befofe passing values. If you passed a string to user_ID, it'll throw an error
+    
+    output = {
+        "Instantiated Model": model,
+        "User ID": "ID_" + str(user_ID),
+        "Confidence Threshold": treshold,
+        "Model Prediction": {
+            "Churn Risk": "High",
+            "Model Performance": "threshold"
+        }
+    }
+
+    return output
+    # try running "/predict/v1?user_ID=88&threshold=0.3"
+
+
+
+
+
+
+
